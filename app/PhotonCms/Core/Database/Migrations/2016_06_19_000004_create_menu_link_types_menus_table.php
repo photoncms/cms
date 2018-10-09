07d56d040e0e43c7daf5331e0585ffcd0c532fce
Tihomir Opacic <tihomir.opacic@orangehilldev.com>
2018-10-09 21:59:11 +0200

    Implementation of force deleteion

--- app/PhotonCms/Core/Database/Migrations/2016_06_19_000004_create_menu_link_types_menus_table.php
+++ app/PhotonCms/Core/Database/Migrations/2016_06_19_000004_create_menu_link_types_menus_table.php
@@ -14,8 +14,8 @@ class CreateMenuLinkTypesMenusTable extends Migration
     public function up()
     {
         Schema::create('menu_link_types_menus', function (Blueprint $table) {
-            $table->integer('menu_id')->unsigned()->index();
-            $table->integer('menu_link_type_id')->unsigned()->index();
+            $table->integer('menu_id')->unsigned()->nullable()->index();
+            $table->integer('menu_link_type_id')->unsigned()->nullable()->index();
 
             $table->foreign('menu_link_type_id')->references('id')->on('menu_link_types')->onDelete('set null');
             $table->foreign('menu_id')->references('id')->on('menus')->onDelete('set null');
